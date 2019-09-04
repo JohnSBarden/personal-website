9687f778a45e0aea17d68661e662f82c13e48ee0
John <john.barden@outlook.com>
2019-09-04 18:50:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LinkedIn from "./LinkedIn";
+import GitHub from "./GitHub";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import "./Carousel/Carousel.scss";
+import { Carousel } from "./Carousel/Carousel";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// RANDOM PIC
+// https://source.unsplash.com/collection/3508426/480x480
