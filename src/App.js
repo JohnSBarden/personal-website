10e770960fdc993e7d617ba12c4ee30c1f351268
John <john.barden@outlook.com>
2020-04-13 16:19:24 -0500

    switched to inline codepen for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.scss";
 import { MyCarousel } from "./Components/Carousel/MyCarousel";
-import { CodePen2 } from "./Components/Playgrounds/CodePen2";
-// import { readFile } from "./scripts/ReadFile";
-// import { FnCarousel } from "./Components/Carousel/Carousel";
+// import { CodePen2 } from "./Components/Playgrounds/CodePen2";
+import { CodePen } from "./Components/Playgrounds/CodePen";
 
 function App(props) {
   console.log("props in app ", props);
-  var location = ""; //"https://codepen.io/johnsbarden/pen/BaoyJOB";
+  // var location = ""; //"https://codepen.io/johnsbarden/pen/BaoyJOB";
   return (
     <div className="App">
       <MyCarousel />
-      <CodePen2 id="playground" script={props.text} location={location} />
+      <CodePen />
+      {/*<CodePen2 id="playground" script={props.text} location={location} />*/}
     </div>
   );
 }
--- src/Components/Playgrounds/CodePen2.js
+++ src/Components/Playgrounds/CodePen2.js
@@ -1,15 +1,10 @@
-import {
-  PrefillEmbed,
-  PrefillLang,
-  useCodePenEmbed,
-  stripIndent,
-} from "react-codepen-prefill-embed";
-import React, { useState } from "react";
+import { PrefillEmbed, useCodePenEmbed } from "react-codepen-prefill-embed";
+import React from "react";
 import { CodePen } from "./CodePen";
 
 function CodePen2(props) {
   useCodePenEmbed();
-  if (props.location != "") {
+  if (props.location !== "") {
     return <PrefillEmbed />;
   } else {
     return <CodePen />;
