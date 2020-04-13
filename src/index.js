0eb343c2f7d3322a2247a6845296f5bf1b036a85
John <john.barden@outlook.com>
2020-04-13 16:33:08 -0500

    removed comments and unused vars in index

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { readFile } from "./scripts/ReadFile";
 
-// var text = readFile("./Components/Playgrounds/default.txt");
 var text;
-// app.get("/getFileContents", function (request, response) {
-//   var text = readFile("./Components/Playgrounds/default.txt");
-//   response.send(text);
-// });
 
 ReactDOM.render(<App text={text} />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
